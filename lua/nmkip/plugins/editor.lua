5d0190ccf9712681bce2ac10b1bc32f86faa1ca1
Juan Ignacio Lopez <juan.ignacio848@gmail.com>
2024-05-13 23:10:58 -0300

    Fix not loaded todo-comments

--- lua/nmkip/plugins/editor.lua
+++ lua/nmkip/plugins/editor.lua
@@ -426,13 +426,24 @@ return {
     ft = "qf"},
   {
     "folke/todo-comments.nvim",
+    event = "VeryLazy",
     dependencies = { "nvim-lua/plenary.nvim", "folke/trouble.nvim" },
-    opts = {
-      highlight = {
-        multiline = false,
-        after = ""
-      }
-    },
+    config = function()
+        local todo = require("todo-comments")
+
+        todo.setup({
+        -- list of named colors where we try to extract the guifg from the
+        -- list of highlight groups or use the hex color if hl not found as a fallback
+        colors = {
+          error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+          warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+          info = { "DiagnosticInfo", "#2563EB" },
+          hint = { "DiagnosticHint", "#10B981" },
+          default = { "Identifier", "#7C3AED" },
+          test = { "Identifier", "#FF00FF" },
+        },
+      })
+    end,
     keys = {
       { "<leader>st", "<cmd>TodoTrouble<cr>", desc = "Open TodoTrouble" },
       { "<leader>sT", "<cmd>TodoTelescope<cr>", desc = "Open TodoTelescope" },
