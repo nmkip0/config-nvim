5d62f0875ff1c236b8abd0ec43cd1421cbe5e923
Juan Ignacio Lopez <juan.ignacio848@gmail.com>
2024-05-14 19:28:51 -0300

    Window resize hydra

---
+++ lua/nmkip/hydras/window.lua
@@ -0,0 +1,28 @@
+return {
+  create = function()
+    local Hydra = require("hydra")
+    Hydra({
+      name = "Resize",
+      mode = "n",
+      config = {
+        invoke_on_body = true,
+        hint = {
+          type = "window",
+          border = "double",
+        },
+      },
+      body = "<leader>w.",
+      heads = {
+        { "=", "<C-W>=", { desc = "Equalize windows" } },
+
+        { "h", "5<C-w>>", { desc = "resize ←" } },
+        { "l", "5<C-w><", { desc = "resize →" } },
+        { "k", "5<C-W>+", { desc = "resize ↑" } },
+        { "j", "5<C-W>-", { desc = "resize ↓" } },
+
+        { "<Esc>", nil, { desc = "Exit", exit = true } },
+        { "q", nil, { desc = "Exit", exit = true } },
+      },
+    })
+  end,
+}
